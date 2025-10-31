bf4eeab9fe98a18a38b9f5b438f4fdf9f9a739fd
BimoJena <taskbimo@gmail.com>
2025-10-31 14:39:58 +0530

    fix: added DJANGO_SETTINGS_MODULE in create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -1,8 +1,15 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpleblog.settings')
+django.setup()
+
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
-if not User.objects.filter(username="bimochanBlog").exists():
-    User.objects.create_superuser("bimochanBlog", "taskbimo@gmail.com", "24mcafsd0008")
-    print("✅ Superuser created!")
+if not User.objects.filter(username='bimochanBlog').exists():
+    User.objects.create_superuser('bimochanBlog', 'taskbimo@gmail.com', '24mcafsd0008')
+    print("Superuser created successfully!")
 else:
-    print("ℹ️ Superuser already exists.")
+    print("Superuser already exists!")
